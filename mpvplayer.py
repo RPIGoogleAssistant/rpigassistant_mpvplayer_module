bcb299cbba55532b325a8689f37432d77db05dc5
APU V <theapu@gmail.com>
2018-02-16 00:50:09 +0530

    Added Skip controls

--- mpvplayer.py
+++ mpvplayer.py
@@ -6,6 +6,11 @@ import psutil
 from settings import readsettings
 from settings import writesettings
 
+#Global variable used for track skipping
+global skipamount 
+skipamount = 0
+
+#Check if mpv player is playing
 def ismpvplayerplaying():
     for pid in psutil.pids():
         p=psutil.Process(pid)
@@ -16,39 +21,62 @@ def ismpvplayerplaying():
             mpvactive=False
     return mpvactive
 
-#Function to manage mpv start volume
+#Function get mpv player volume from mpvplayer.json
 def mpvplayergetvolume():
     mpvvolume=readsettings('mpvplayer','volume')
     return mpvvolume
 
+#Adjust mpv player volume temporarily (not saved to mpvplayer.json)
+#restore to previous value using mpvplayerrestorevolume
 def mpvplayeradjustvolume(mpvvolumelevel):
     if ismpvplayerplaying():
        if mpvvolumelevel < mpvplayergetvolume():
           os.system('echo \'{ "command": ["set_property", "volume", "'+ mpvvolumelevel +'"] }\' | socat - /tmp/mpvsocket')
 
+#Restore to volume set in mpvplayer.json
 def mpvplayerrestorevolume(mpvvolumelevel):
     if ismpvplayerplaying():
        os.system('echo \'{ "command": ["set_property", "volume", "'+ mpvvolumelevel +'"] }\' | socat - /tmp/mpvsocket')
 
+#Permanenlty set volume to mpvplayer.json
 def mpvplayersetvolume(mpvvolumelevel):
     if ismpvplayerplaying():
        os.system('echo \'{ "command": ["set_property", "volume", "'+ mpvvolumelevel +'"] }\' | socat - /tmp/mpvsocket')
     writesettings('mpvplayer','volume',mpvvolumelevel)
 
+#Mute
 def mpvplayermute():
     mpvplayersetvolume("0")
 
+#Unmute
 def mpvplayerunmute():
     mpvplayersetvolume("70")
 
+#Play from url with volume level mpvvolume
 def mpvplayer(mpvvolume,mediaurl):
     mediaurl=("'"+mediaurl+"'")
     os.system('mpv --really-quiet --volume='+str(mpvvolume)+' '+ mediaurl)
 
+#Stop mpv player
 def mpvplayerstop():
     if ismpvplayerplaying():
        os.system('echo \'{"command": ["quit"]}\' | socat - /tmp/mpvsocket')
 
+#Pause/Unpause
 def mpvplayercycle():
     if ismpvplayerplaying():
        os.system('echo \'{"command": ["cycle", "pause"]}\' | socat - /tmp/mpvsocket')
+
+#Stop mpvplayer and set Global variable skipamount
+def mpvplayerskip(skipby):
+    mpvplayersetskip(skipby)
+    mpvplayerstop()
+
+#Get how many tracks to skip
+def mpvplayergetskip():
+    return int(skipamount)
+
+#Set how many tracks to skip
+def mpvplayersetskip(skipby):
+    global skipamount
+    skipamount = int(skipby)
